41df56af5f6bd737edb3f1e780f4fe30736e153e
Ababaki <124071121+Ababaki@users.noreply.github.com>
2023-04-16 17:09:45 -0400

    front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('');
+  const [reply, setReply] = useState('');
+
+  const handleChange = (e) => {
+    setMessage(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post('http://localhost:5000/api/chat', { message });
+      setReply(response.data.reply);
+    } catch (error) {
+      console.error(error);
+      setReply('Error fetching response');
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>ChatGPT Chatbox</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="message">Message:</label>
+        <input
+          type="text"
+          id="message"
+          value={message}
+          onChange={handleChange}
+        />
+        <button type="submit">Send</button>
+      </form>
+      <h2>Reply from ChatGPT:</h2>
+      <p>{reply}</p>
     </div>
   );
 }
